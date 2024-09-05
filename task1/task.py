7d6a98b4456c2d320983f929ed2f8d14c5575119
Ivan Solovev <msksolovev93@mail.ru>
2024-09-05 17:21:17 +0300

    Done task1

--- task1/task.py
+++ task1/task.py
@@ -1,5 +1,20 @@
-def main():
-    print(1)
+import json
+import numpy
+
+def main(path):
+    with open(path, "r") as file:
+        data = json.load(file)
+    nodes = data["nodes"]
+    print("Graph in json string format:\n", nodes, "\n")
+    matrix = numpy.zeros((len(nodes), len(nodes)), int)
+    for source in nodes:
+        for target in nodes[source]:
+            matrix[int(source)-1, int(target)-1] = 1
+            matrix[int(target)-1, int(source)-1] = 1
+
+    print("Adjacency matrix:\n", matrix)
+    
 
 if __name__ == "__main__":
-    main()
+    path = "./task1/graph.json"
+    main(path)
